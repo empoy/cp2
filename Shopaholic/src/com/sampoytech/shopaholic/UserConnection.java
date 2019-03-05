af4ab72a197ab82ff59375d56aa179a307ec0658
Sam <x.cancel@gmail.com>
2019-03-05 21:46:16 +0300

    delete and add method

--- Shopaholic/src/com/sampoytech/shopaholic/UserConnection.java
+++ Shopaholic/src/com/sampoytech/shopaholic/UserConnection.java
@@ -1,6 +1,13 @@
 package com.sampoytech.shopaholic;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class UserConnection {
 	
@@ -9,6 +16,7 @@ public class UserConnection {
 	String f="Login.txt";
 	String tempF="Login_temp.txt";
 	File file=new File(f);
+	File fileT=new File(tempF);
 	public UserConnection()
 	{}
 	public UserConnection(String id,String username,String password,String name,
@@ -24,10 +32,56 @@ public class UserConnection {
 		this.address=address;
 	}
 	
-	public Boolean userAdder(String Id) {
-		
+	public Boolean userAdder(String id,String username,String password,String name,
+			String surname,String accessLevel,String number,String address) throws IOException 
+	{
+		BufferedReader br=new BufferedReader(new FileReader(file) );
+		String check = id;
+		String currentLine;
+		while((currentLine = br.readLine()) != null) 
+		{
+			String[] parts =currentLine.split(" ");
+		    if(parts[0].equals(check)) 
+		    {
+		    	br.close();
+		    	return false;
+		    }
+		    else
+		    {
+		    	PrintWriter pw=new PrintWriter(new FileWriter(file,true));
+				pw.println(id+" "+username+" "+password+" "+name
+						+" "+surname+" "+accessLevel+" "+number+" "+address);
+				pw.close();
+				br.close();
+				return true;
+		    }
+		}
+		br.close();
 		return null;
+	}
+	
+	public Boolean deleteUser(String id) throws IOException 
+	{
+		BufferedReader br=new BufferedReader(new FileReader(file));
+		BufferedWriter bw=new BufferedWriter(new FileWriter(fileT));
 		
+		String lineToRemove = id;
+		String currentLine;
+
+		while((currentLine = br.readLine()) != null) {
+			String[] parts =currentLine.split(" ");
+		    if(parts[0].equals(lineToRemove)) continue;
+		    bw.write(currentLine + System.getProperty("line.separator"));
+		}
+		bw.close(); 
+		br.close(); 
+		file.setWritable(true);
+		fileT.setWritable(true);
+		boolean successful1=file.delete();
+		boolean successful = fileT.renameTo(file);
+		if (successful==true && successful1==true) 
+		return successful;
+		return false;
 	}
 
 }
