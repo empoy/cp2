769dc70b810c54276d14324b53416bf06af51e25
Sam <x.cancel@gmail.com>
2019-04-22 22:57:25 +0300

    test

--- Shopaholic/src/com/sampoytech/shopaholic/Connection.java
+++ Shopaholic/src/com/sampoytech/shopaholic/Connection.java
@@ -10,5 +10,5 @@ public abstract class Connection
 	abstract Boolean delete(String id) throws IOException;
 	abstract Object lister() throws IOException;
 	abstract Object select(String id) throws IOException;
-	///
+	//
 }
